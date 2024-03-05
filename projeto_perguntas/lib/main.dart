5a4e4c74bc4b74da607aa588a0721ef82c6854ec
@racinefell <102471647+racinefe@users.noreply.github.com>
2024-03-05 14:39:22 -0300

    Aplicando Estilo ao Widget

--- projeto_perguntas/lib/main.dart
+++ projeto_perguntas/lib/main.dart
@@ -1,26 +1,23 @@
-
 // ignore_for_file: prefer_const_constructors, avoid_print, library_private_types_in_public_api
 
 import 'package:flutter/material.dart';
 import './questao.dart';
 
-main() => runApp( MyApp());
+main() => runApp(MyApp());
 
-
-class _PerguntaAppState extends State<MyApp>{
-
+class _PerguntaAppState extends State<MyApp> {
   var _perguntaSelecionada = 0;
 
   void _responder() {
     setState(() {
       _perguntaSelecionada++;
     });
-    
+
     print(_perguntaSelecionada);
   }
 
   @override
-  Widget  build (BuildContext context) {
+  Widget build(BuildContext context) {
     final perguntas = [
       'Qual é a principal diferença entre células procarióticas e eucarióticas?',
       'Qual é o órgão responsável pela troca gasosa nos seres humanos?',
@@ -30,37 +27,33 @@ class _PerguntaAppState extends State<MyApp>{
       key: UniqueKey(),
       home: Scaffold(
         appBar: AppBar(
-            title: const Center(
-            child: Text('Perguntas')
-          ),
+          title: const Center(child: Text('Perguntas')),
         ),
-        body:  Column(
-          children: <Widget> [
-             Questao(perguntas[_perguntaSelecionada]),
-              ElevatedButton(
-              onPressed: _responder, 
-              child:  Text('(A) Ambas possuem núcleo.')
-             ),
-             ElevatedButton(
-              onPressed: _responder, 
-              child:  Text('(B) Células procarióticas possuem núcleo, e células eucarióticas não possuem.')
-             ),
-             ElevatedButton(
-              onPressed: _responder, 
-              child:  Text('(C)  Células eucarióticas possuem núcleo, e células procarióticas não possuem.')
-             ),
-             ElevatedButton(
-              onPressed: _responder, 
-              child:  Text('(D) Nenhuma das opções anteriores.')
-             ),
+        body: Column(
+          children: <Widget>[
+            Questao(perguntas[_perguntaSelecionada]),
+            ElevatedButton(
+                onPressed: _responder,
+                child: Text('(A) Ambas possuem núcleo.')),
+            ElevatedButton(
+                onPressed: _responder,
+                child: Text(
+                    '(B) Células procarióticas possuem núcleo, e células eucarióticas não possuem.')),
+            ElevatedButton(
+                onPressed: _responder,
+                child: Text(
+                    '(C)  Células eucarióticas possuem núcleo, e células procarióticas não possuem.')),
+            ElevatedButton(
+                onPressed: _responder,
+                child: Text('(D) Nenhuma das opções anteriores.')),
           ],
         ),
       ),
     );
   }
-
 }
-class MyApp extends  StatefulWidget {
+
+class MyApp extends StatefulWidget {
   const MyApp({super.key});
   @override
   _PerguntaAppState createState() {
--- projeto_perguntas/lib/questao.dart
+++ projeto_perguntas/lib/questao.dart
@@ -1,16 +1,20 @@
-
-
 import 'package:flutter/material.dart';
 
 class Questao extends StatelessWidget {
-  
   final String texto;
 
   const Questao(this.texto, {super.key});
 
-  
   @override
   Widget build(BuildContext context) {
-    return Text(texto);
+    return Container(
+      width: double.infinity,
+      margin: const EdgeInsets.all(20),
+      child: Text(
+        texto,
+        style: const TextStyle(fontSize: 20),
+        textAlign: TextAlign.center,
+      ),
+    );
   }
 }
