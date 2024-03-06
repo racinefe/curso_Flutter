292194ffc884ffb440f88ccf26ccb5ddb25a4572
@racinefell <102471647+racinefe@users.noreply.github.com>
2024-03-06 14:33:13 -0300

    criando lista para widgets

--- projeto_perguntas/lib/resposta.dart
+++ projeto_perguntas/lib/resposta.dart
@@ -1,18 +1,19 @@
-import 'dart:ffi';
-
 import 'package:flutter/material.dart';
 
 class Resposta extends StatelessWidget {
   final String texto;
-  const Resposta(this.texto, {super.key});
+  final void Function() onselected;
+  
+  const Resposta(this.texto, this.onselected, {super.key});
 
   @override
   Widget build(BuildContext context) {
     return Container(
       width: double.infinity,
-      margin: const EdgeInsets.all(3),
+      margin: const EdgeInsets.fromLTRB(50, 5, 50, 5),
+      padding: const EdgeInsets.fromLTRB(5, 0, 5, 0),
       child: ElevatedButton(
-        onPressed: null,
+        onPressed: onselected,
         child: Text(texto),
       ),
     );
