07ea29ea8ab51555ea3dcadabadd9e20210eb322
@racinefell <102471647+racinefe@users.noreply.github.com>
2024-03-12 17:48:27 -0300

    adicionando pontuação

--- projeto_perguntas/lib/resultado.dart
+++ projeto_perguntas/lib/resultado.dart
@@ -1,14 +1,27 @@
 import 'package:flutter/material.dart';
 
 class Resultado extends StatelessWidget {
-  const Resultado({super.key});
+  const Resultado(this.pontuacao, {super.key});
+  final int pontuacao;
+
+  String get fraseResultado {
+    if (pontuacao < 10) {
+      return 'pontuação ${pontuacao.toString()}\n Vamos Estudar mais!';
+    } else if (pontuacao < 20) {
+      return 'pontuação ${pontuacao.toString()}\n Precisa Melhora!';
+    } else if (pontuacao < 30) {
+      return 'pontuação ${pontuacao.toString()}\n Isso foi Bom!!';
+    } else {
+      return 'pontuação ${pontuacao.toString()}\n Que Incrivel!';
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
-    return const Center(
+    return Center(
       child: Text(
-        ' (>ô_õ)> Parabaiinnsss!!!',
-        style: TextStyle(fontSize: 30),
+        fraseResultado,
+        style: const TextStyle(fontSize: 30),
       ),
     );
   }
